1975f1433fcd7c2517d494638cfe6fbd75ee6a55
tyxlb <91867477+tyxlb@users.noreply.github.com>
2026-02-28 18:25:40 +0800

    refactor: wrap code in class

--- client.py
+++ client.py
@@ -1,75 +1,68 @@
 import RNS
 import time
 from fastapi import FastAPI, Response, HTTPException
-from contextlib import asynccontextmanager
 import uvicorn
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    reticulum = RNS.Reticulum()
-    yield
+class client:
+    def __init__(self):
+        RNS.Reticulum()
+        self.app = FastAPI()
 
-
-app = FastAPI(lifespan=lifespan)
-
-links: dict[str, RNS.Link] = {}
-
-
-def get_link(destination_hexhash):
-    """see https://reticulum.network/manual/examples.html"""
-    if destination_hexhash not in links:
-        dest_len = (RNS.Reticulum.TRUNCATED_HASHLENGTH // 8) * 2
-        if len(destination_hexhash) != dest_len:
-            raise ValueError(
-                "Destination length is invalid, must be {hex} hexadecimal characters ({byte} bytes).".format(
-                    hex=dest_len, byte=dest_len // 2
-                )
-            )
-        destination_hash = bytes.fromhex(destination_hexhash)
-        if not RNS.Transport.has_path(destination_hash):
-            RNS.log(
-                "Destination is not yet known. Requesting path and waiting for announce to arrive..."
-            )
-            RNS.Transport.request_path(destination_hash)
-            while not RNS.Transport.has_path(destination_hash):
+        @self.app.get("/{destination_hexhash}/files/{file_path:path}")
+        def read_file(destination_hexhash: str, file_path: str):
+            link = self.get_link(destination_hexhash)
+            file: RNS.RequestReceipt = link.request("/files", file_path.encode("utf-8"))
+            while not file.concluded():
                 time.sleep(0.1)
-        server_identity = RNS.Identity.recall(destination_hash)
-        RNS.log("Establishing link with server...")
-        server_destination = RNS.Destination(
-            server_identity,
-            RNS.Destination.OUT,
-            RNS.Destination.SINGLE,
-            "live",
-            "stream",
-        )
-        link = RNS.Link(server_destination)
-        link.set_link_closed_callback(link_closed)
-        links[destination_hexhash] = link
-    while links[destination_hexhash].status != RNS.Link.ACTIVE:
-        time.sleep(0.1)
-    return links[destination_hexhash]
+            return Response(file.get_response())
+
+        self.links: dict[str, RNS.Link] = {}
+        uvicorn.run(self.app)
+
+    def get_link(self, destination_hexhash):
+        """see https://reticulum.network/manual/examples.html"""
+        if destination_hexhash not in self.links:
+            dest_len = (RNS.Reticulum.TRUNCATED_HASHLENGTH // 8) * 2
+            if len(destination_hexhash) != dest_len:
+                raise ValueError(
+                    "Destination length is invalid, must be {hex} hexadecimal characters ({byte} bytes).".format(
+                        hex=dest_len, byte=dest_len // 2
+                    )
+                )
+            destination_hash = bytes.fromhex(destination_hexhash)
+            if not RNS.Transport.has_path(destination_hash):
+                RNS.log(
+                    "Destination is not yet known. Requesting path and waiting for announce to arrive..."
+                )
+                RNS.Transport.request_path(destination_hash)
+                while not RNS.Transport.has_path(destination_hash):
+                    time.sleep(0.1)
+            server_identity = RNS.Identity.recall(destination_hash)
+            RNS.log("Establishing link with server...")
+            server_destination = RNS.Destination(
+                server_identity,
+                RNS.Destination.OUT,
+                RNS.Destination.SINGLE,
+                "live",
+                "stream",
+            )
+            link = RNS.Link(server_destination)
+            link.set_link_closed_callback(self.link_closed)
+            self.links[destination_hexhash] = link
+        while self.links[destination_hexhash].status != RNS.Link.ACTIVE:
+            time.sleep(0.1)
+        return self.links[destination_hexhash]
+
+    def link_closed(self, link: RNS.Link):
+        if link.teardown_reason == RNS.Link.TIMEOUT:
+            RNS.log("The link timed out, exiting now")
+        elif link.teardown_reason == RNS.Link.DESTINATION_CLOSED:
+            RNS.log("The link was closed by the server, exiting now")
+        else:
+            RNS.log("Link closed, exiting now")
+        self.links.pop(RNS.prettyhexrep(link.destination.hash)[1:-1])
 
 
-def link_closed(link: RNS.Link):
-    if link.teardown_reason == RNS.Link.TIMEOUT:
-        RNS.log("The link timed out, exiting now")
-    elif link.teardown_reason == RNS.Link.DESTINATION_CLOSED:
-        RNS.log("The link was closed by the server, exiting now")
-    else:
-        RNS.log("Link closed, exiting now")
-    links.pop(RNS.prettyhexrep(link.destination.hash)[1:-1])
-
-
-@app.get("/{destination_hexhash}/files/{file_path:path}")
-def read_file(destination_hexhash: str, file_path: str):
-    link = get_link(destination_hexhash)
-    file: RNS.RequestReceipt = link.request("/files", file_path.encode("utf-8"))
-    while not file.concluded():
-        time.sleep(0.1)
-    return Response(file.get_response())
-
-
-uvicorn.run(
-    app,
-)
+if __name__ == "__main__":
+    client()
--- server.py
+++ server.py
@@ -1,66 +1,67 @@
 import RNS
 from pathlib import Path
 
-serve_path = Path("./files")
-configpath = Path("./config")
-identitypath = configpath.joinpath("/identity")
 
+class server:
+    def __init__(self, filespath="./files", configpath="./config"):
+        self.filespath = Path(filespath)
+        self.configpath = Path(configpath)
+        self.identitypath = self.configpath.joinpath("/identity")
+        self.filespath.mkdir(parents=True, exist_ok=True)
+        self.configpath.mkdir(parents=True, exist_ok=True)
 
-def main():
-    serve_path.mkdir(parents=True, exist_ok=True)
-    configpath.mkdir(parents=True, exist_ok=True)
+        RNS.Reticulum()
 
-    reticulum = RNS.Reticulum()
+        if self.identitypath.is_file():
+            self.identity = RNS.Identity.from_file(self.identitypath)
+            RNS.log("Loaded identity from file", RNS.LOG_INFO)
+        else:
+            RNS.log("No Primary Identity file found, creating new...", RNS.LOG_INFO)
+            self.identity = RNS.Identity()
+            self.identity.to_file(self.identitypath)
 
-    if identitypath.is_file():
-        identity = RNS.Identity.from_file(identitypath)
-        RNS.log("Loaded identity from file", RNS.LOG_INFO)
-    else:
-        RNS.log("No Primary Identity file found, creating new...", RNS.LOG_INFO)
-        identity = RNS.Identity()
-        identity.to_file(identitypath)
-
-    server_destination = RNS.Destination(
-        identity,
-        RNS.Destination.IN,
-        RNS.Destination.SINGLE,
-        "live",
-        "stream",
-    )
-    server_destination.register_request_handler(
-        "/files", response_generator=files_generator, allow=RNS.Destination.ALLOW_ALL
-    )
-    server_destination.set_link_established_callback(client_connected)
-    RNS.log(f"Address: {RNS.prettyhexrep(server_destination.hash)}", RNS.LOG_INFO)
-
-    server_loop(server_destination)
-
-
-def client_connected(link: RNS.Link):
-    pass
-
-
-def files_generator(path, data, request_id, link_id, remote_identity, requested_at):
-    try:
-        filename = data.decode("utf-8")
-        with open(serve_path.joinpath(filename), "rb") as data_file:
-            content = data_file.read()
-            return content
-    except Exception as e:
-        RNS.log(
-            f"Error while generating response to request {RNS.prettyhexrep(request_id)} on link {RNS.prettyhexrep(link_id)}, {e}",
-            RNS.LOG_ERROR,
+        self.destination = RNS.Destination(
+            self.identity,
+            RNS.Destination.IN,
+            RNS.Destination.SINGLE,
+            "live",
+            "stream",
         )
-        return b""
+        self.destination.register_request_handler(
+            "/files",
+            response_generator=self.files_generator,
+            allow=RNS.Destination.ALLOW_ALL,
+        )
+        self.destination.set_link_established_callback(self.client_connected)
+        RNS.log(f"Address: {RNS.prettyhexrep(self.destination.hash)}", RNS.LOG_INFO)
 
+        self.server_loop()
 
-def server_loop(destination):
-    RNS.log("Hit enter to manually send an announce (Ctrl-C to quit)")
-    while True:
-        entered = input()
-        destination.announce()
-        RNS.log("Sent announce from " + RNS.prettyhexrep(destination.hash))
+    def client_connected(self, link: RNS.Link):
+        pass
+
+    def files_generator(
+        self, path, data, request_id, link_id, remote_identity, requested_at
+    ):
+        try:
+            filename = data.decode("utf-8")
+            with open(self.filespath.joinpath(filename), "rb") as data_file:
+                content = data_file.read()
+                return content
+        except Exception as e:
+            RNS.log(
+                f"Error while generating response to request {RNS.prettyhexrep(request_id)} on link {RNS.prettyhexrep(link_id)}, {e}",
+                RNS.LOG_ERROR,
+            )
+            return b""
+
+    def server_loop(self):
+        RNS.log("Hit enter to manually send an announce (Ctrl-C to quit)")
+        while True:
+            entered = input()
+            self.destination.announce()
+            RNS.log("Sent announce from " + RNS.prettyhexrep(self.destination.hash))
 
 
 if __name__ == "__main__":
-    main()
+    server()
